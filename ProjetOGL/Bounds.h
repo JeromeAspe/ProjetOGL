132daa8420ebd3cccce90ddd59ef083bd3adfaf7
LudovicHanin <HorizonMer5737@gmail.com>
2021-03-02 14:07:15 +0100

    Bounds + Collider

--- ProjetOGL/Bounds.h
+++ ProjetOGL/Bounds.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
 
 
 class Bounds
@@ -6,5 +9,20 @@ class Bounds
 protected:
 	glm::vec3 center = glm::vec3(0, 0, 0);
 	glm::vec3 size = glm::vec3(0, 0, 0);
+
+public:
+	/*----Getter----*/
+	inline glm::vec3 GetCenter() const { return center; };
+	inline glm::vec3 GetSize() const { return size; };
+
+	/*----Size----*/
+	inline void SetCenter(glm::vec3 _center) { center = _center; };
+	inline void SetSize(glm::vec3 _size) { size = _size; };
+
+	/*----Constructor----*/
+	Bounds(const glm::vec3 _center, const glm::vec3 _size);
+
+	/*----Methods----*/
+	bool Intersects(const Bounds& _bounds);
 };
 
--- ProjetOGL/Collider.cpp
+++ ProjetOGL/Collider.cpp
@@ -1,2 +1,11 @@
 #include "Collider.h"
 
+Collider::Collider()
+{
+}
+
+Collider::Collider(GameObject* _attachedObejct)
+{
+	attachedObject = _attachedObejct;
+	bounds = new Bounds(attachedObject->GetTransform()->GetPosition(), attachedObject->GetTransform()->GetScale());
+}
